7c59c09215ac44a715c80221b150ef2fdae86bab
aaltaf65 <aqueelnazim65@gmail.com>
2025-08-16 19:33:56 -0400

    Added comments and print statement to "Begin here"

---
+++ Begin here.py
@@ -0,0 +1,9 @@
+
+
+# Hello everyone! This is a simple Python script that prints "Hello, World!" to the console.
+
+print("Hello, World!")
+# This is a basic starting point for anyone learning Python.
+# You can run this script in any Python environment to see the output.
+# Feel free to modify the message and experiment with other print statements!
+# Enjoy coding!
